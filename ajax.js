b65daa93d4bd1455ecb2f37fe1f7225926459155
maryfranks <maryfranks@gmail.com>
2017-08-21 14:39:54 -0400

    add button to submit request to pong

--- ajax.js
+++ ajax.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var rootButton = document.getElementById("root-request");
+  var pingPong = document.getElementById("ping-pong");
 
   rootButton.addEventListener("click", function() {
     $.ajax({
@@ -8,6 +9,25 @@ document.addEventListener("DOMContentLoaded", function() {
       method: "GET",
       data: {},
       dataType: "text"
+    });
+  });
+
+  pingPong.addEventListener("click", function() {
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/ping",
+      method: "GET",
+      dataType: "text"
+    }).done (function (responseData) {
+      console.log(responseData);
+      var newElement = document.createElement('p');
+      newElement.innerText = responseData;
+      document.getElementById("step3456").append(newElement);
+    }).fail (function() {
+      var newElement = document.createElement('p')
+      newElement.innerText = "Whoops!  Something went wrong.";
+      document.getElementById("step3456").append(newElement);
+    }).always(function() {
+      console.log("Request finished");
     });
   });
 
